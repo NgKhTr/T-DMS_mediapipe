76015b7103e8a04c6a8f8befe433fadc8cff3de1
NgKhTr <ktrinh1032003@gmail.com>
2025-11-05 15:41:39 +0700

    adj: add connect speaker script

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+sleep 5
 cd /home/nextwave/Desktop/T-DMS/T-DMS_mediapipe
+pactl set-default-sink alsa_output.usb-Generic_USB2.0_Device_20121120222016-00.analog-stereo
+sleep 5
 GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/T-DMS/main_cpu --calculator_graph_config_file=mediapipe/graphs/T-DMS/main.pbtxt
